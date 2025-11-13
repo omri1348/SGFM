b6f09d4c591513ce53c58accab238b1d24fcf4cb
Ben <bkmi@users.noreply.github.com>
2025-11-13 17:48:24 +0000

    updated alex_mp_20 to 1250, added automatic testing for alex_mp_20

--- scripts/crystal_pkl.sh
+++ scripts/crystal_pkl.sh
@@ -1,10 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
 items=('mp_20')
-# items=('mp_20' 'alex_mp_20')
+
+if [ -d "data/alex_mp_20" ] && compgen -G "data/alex_mp_20/*.csv" > /dev/null; then
+    # .csv files found, add alex_mp_20 to the list
+    items+=("alex_mp_20")
+fi
 
 for item in "${items[@]}"; do
     echo "Generating crystal pkls for $item"
     uv run scripts/crystal_pkl.py "$item"
 done
-
--- scripts/data_setup.sh
+++ scripts/data_setup.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 items=('perov' 'carbon' 'mp_20' 'mpts_52')
-# items=('perov' 'carbon' 'mp_20' 'mpts_52' 'alex_mp_20')
+
+if [ -d "data/alex_mp_20" ] && compgen -G "data/alex_mp_20/*.csv" > /dev/null; then
+    # .csv files found, add alex_mp_20 to the list
+    items+=("alex_mp_20")
+fi
 
 for item in "${items[@]}"; do
     echo "Processing $item"
